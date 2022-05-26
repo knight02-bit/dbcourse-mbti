ffdcef3f0c3f7d96078423d56caa5de2421dce3b
Knight02 <1605008190@qq.com>
2022-05-26 22:14:34 +0800

    gorm连接数据库,插入新用户,实现注册逻辑

--- GoLearning/ginVueDemo1/go.mod
+++ GoLearning/ginVueDemo1/go.mod
@@ -4,7 +4,8 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.7.7
-	golang.org/x/net v0.0.0-20220524220425-1d687d428aca
+	gorm.io/driver/postgres v1.3.6
+	gorm.io/gorm v1.23.5
 )
 
 require (
@@ -13,6 +14,14 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/pgx/v4 v4.16.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -26,5 +35,4 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gorm.io/gorm v1.23.5 // indirect
 )
--- GoLearning/ginVueDemo1/main.go
+++ GoLearning/ginVueDemo1/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
-	"golang.org/x/net/context/ctxhttp"
+	//"golang.org/x/net/context/ctxhttp"
 	"log"
 	"math/rand"
 	"net/http"
@@ -20,8 +21,7 @@ type User struct {
 }
 
 func main()  {
-	//db := InitDb()
-
+	db := InitDB()
 	//defer db.DB.Close()
 
 	r := gin.Default()
@@ -47,18 +47,18 @@ func main()  {
 		}
 
 		//判断手机号是否存在
-		//if isTeleExit(db, telephone) {
-		//	ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code":422, "msg":"该手机号已存在"})
-		//	return
-		//}
+		if isTeleExit(db, telephone) {
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code":422, "msg":"该手机号已存在"})
+			return
+		}
 
 		// 创建用户
-		//newUser := User{
-		//	Name: name,
-		//	Telephone: telephone,
-		//	Password: password,
-		//}
-		//db.Create(&newUser)
+		newUser := User{
+			Name: name,
+			Telephone: telephone,
+			Password: password,
+		}
+		db.Create(&newUser)
 
 		log.Println(name, telephone, password)
 		ctx.JSON(200, gin.H{
@@ -66,6 +66,16 @@ func main()  {
 		})
 	})
 	panic(r.Run()) // listen and serve on 0.0.0.0:8080
+}
+
+func isTeleExit(db *gorm.DB, tele string) bool{
+	var user User
+	db.Where("telephone = ?", tele).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+
+	return false
 }
 
 func RandomString(n int) string {
@@ -80,3 +90,31 @@ func RandomString(n int) string {
 	return string(result)
 }
 
+func InitDB() *gorm.DB{
+	// 开启连接池
+	//driverName := "postgres"
+	//host := "localhost"
+	//port := "5432"
+	//database := "ginessential"
+	//username := "postgres"
+	//password := "manager"
+	//charset := "utf8"
+	//args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+	//	username,
+	//	password,
+	//	host,
+	//	port,
+	//	database,
+	//	charset)
+	dsn := "host=localhost " +
+		"user=postgres password=manager " +
+		"dbname=ginessential port=5432 " +
+		"sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("fail to connect, err : "+ err.Error())
+	}
+
+	db.AutoMigrate(&User{})
+	return db
+}
