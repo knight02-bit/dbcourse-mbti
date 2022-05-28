fe7d24d3a191343490cd643a5e45687064d59817
Knight02 <1605008190@qq.com>
2022-05-28 09:54:51 +0800

    gozero练习:创建user rpc服务

---
+++ gozeroLearning/go-zero-demo/mall/user/rpc/internal/logic/getuserlogic.go
@@ -0,0 +1,24 @@
+package logic
+
+import "context"
+
+type GetUserLogic struct {
+	ctx context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserLogic {
+	return &GetUserLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *GetUserLogic) GetUser(in *user.IdRequest) (*user.UserResponse, error) {
+	return &user.UserResponse{
+		Id:   "1",
+		Name: "test",
+	}, nil
+}
