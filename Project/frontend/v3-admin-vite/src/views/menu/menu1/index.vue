01edd67702753b3e4bef7ebf4da1da292f664ea9
Knight02 <1605008190@qq.com>
2022-06-04 21:25:35 +0800

    答题界面

--- Project/frontend/v3-admin-vite/src/views/menu/menu1/index.vue
+++ Project/frontend/v3-admin-vite/src/views/menu/menu1/index.vue
@@ -46,35 +46,48 @@ const tableData = [
 
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column type="expand">
+    <!-- <el-table-column type="expand">
       <template>
         <div>
           <p>State</p>
         </div>
       </template>
+    </el-table-column> -->
+    <el-table-column label="  👇👇👇">
+      <el-button type="success" style="margin-left: 1px" @click="drawer = true"> 查看详情 </el-button>
     </el-table-column>
     <el-table-column prop="id" label="学号" />
     <el-table-column prop="name" label="姓名" />
     <el-table-column prop="date" label="时间" />
     <el-table-column prop="resString" label="测试结果" />
   </el-table>
-  <!-- <div class="demo-image__preview">
-    <el-image
-      style="width: 100px; height: 100px"
-      src="https://gitee.com/knight-02/dbcourse-mbti/raw/master/Project/frontend/v3-admin-vite/src/views/menu/menu1/ISTG.png"
-    />
-  </div> -->
+
+  <el-drawer v-model="drawer" title="测试结果: ISTG" :with-header="true">
+    <span>
+      {{ tableData[0].about }}
+    </span>
+  </el-drawer>
 </template>
 
 <script lang="ts" setup>
+import { map } from "lodash"
+import { ref } from "vue"
+const drawer = ref(false)
 const tableData = [
   {
     id: "2020082229",
     name: "knight",
     date: "2022年6月2日 15:27:18",
-    resString: "ISTG"
+    resString: "ISTG",
+    about: "balabala"
   }
 ]
+
+const character = {
+  ISTG: "ISTGISTGISTGISTGISTG",
+  abcd: "abcdabcdabcdabcdabcd"
+}
+// map mp["ISTG"] = "HHHHHHH"
 </script>
 
 <style scoped>
