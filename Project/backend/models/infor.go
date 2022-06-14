457c43012b1a13ab3ffcc4d9f4b296dc763fa380
Knight02 <1605008190@qq.com>
2022-06-14 18:35:40 +0800

    go model

---
+++ Project/backend/models/infor.go
@@ -0,0 +1,16 @@
+package models
+
+
+type StudentInfo struct {
+	Sid string `form:"Sid" db:"Sid"`
+	Sname string `form:"Sname" db:"Sname"`
+	CGname string `form:"CGname" db:"CGname"`
+	Dname string `form:"Dname" db:"Dname"`
+	Cid string `form:"Cid" db:"Cid"`
+}
+
+type DeleteStudent struct {
+	Sid string `form:"Sid" db:"Sid"`
+}
+
+
